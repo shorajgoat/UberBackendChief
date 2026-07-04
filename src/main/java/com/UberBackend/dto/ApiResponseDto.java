102c0b2b430049218c83ab0cdbfff8bbf893da0c
Shoraj Ghimire <shorajghm10@gmail.com>
2026-07-04 19:32:18 +0545

    Create ApiResponseDto.java
    For formatted Api Response to the client

---
+++ src/main/java/com/UberBackend/dto/ApiResponseDto.java
@@ -0,0 +1,47 @@
+package com.UberBackend.dto;
+
+public class ApiResponseDto<T> {
+	private String message;
+	private boolean sucess;
+	private T data;
+	
+	
+	
+public ApiResponseDto(String message, boolean sucess, T data) {
+		this.message = message;
+		this.sucess = sucess;
+		this.data = data;
+	}
+
+
+
+public static <T>ApiResponseDto<T>sucess(String message,T data ){
+	return new ApiResponseDto<>(message,true,data);
+}
+public static <T>ApiResponseDto<T>failure(String message){
+	return new ApiResponseDto<>(message,false,null);
+}
+public static <T>ApiResponseDto<T>sucess(String message){
+	return new ApiResponseDto<>(message,true,null);
+}
+
+
+
+public String getMessage() {
+	return message;
+}
+
+
+
+public boolean isSucess() {
+	return sucess;
+}
+
+
+
+public T getData() {
+	return data;
+}
+
+
+}

173da401b794177d177e62876cbcd8970132fc81
Shoraj Ghimire <shorajghm10@gmail.com>
2026-07-04 14:31:49 +0545

    Update User.java

--- src/main/java/com/UberBackend/Entity/User.java
+++ src/main/java/com/UberBackend/Entity/User.java
@@ -4,7 +4,13 @@ import java.time.LocalDateTime;
 
 import com.UberBackend.enums.Role;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -12,11 +18,19 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name="users")
 public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
+	@Column(nullable=false,unique=true)
 	private String phoneNo;
+	@Column(nullable=false,unique=true)
 	private String email;
+	@Column(nullable=false)
 	private String name;
+	@Column(nullable=false)
+	@Enumerated(EnumType.STRING)
 	private Role role;
+	@Column(nullable=false)
 	private String password;
 	private LocalDateTime createdAt;
 	private LocalDateTime updatedAt;

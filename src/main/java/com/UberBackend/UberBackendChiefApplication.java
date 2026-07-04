4bdc286dc6dc493645dfe8bae531b7e68d6635d5
Shoraj Ghimire <shorajghm10@gmail.com>
2026-07-04 10:41:03 +0545

    Initial commit

---
+++ src/main/java/com/UberBackend/UberBackendChiefApplication.java
@@ -0,0 +1,13 @@
+package com.UberBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberBackendChiefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberBackendChiefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/UberBackend/UberBackendChiefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UberBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberBackendChiefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

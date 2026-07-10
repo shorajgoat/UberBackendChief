b74bde8d8b5d32b30a62ef00301bdd70e2837eb6
Shoraj Ghimire <shorajghm10@gmail.com>
2026-07-10 19:42:09 +0545

    Create Vehicle.java

---
+++ src/main/java/com/UberBackend/Entity/Vehicle.java
@@ -0,0 +1,67 @@
+package com.UberBackend.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="vehicle")
+public class Vehicle {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	@OneToOne
+	@JoinColumn(name="driver_profile_id")
+	private DriverProfile driverProfile;
+	@Column(nullable=false)
+	private String model;
+	@Column(nullable=false)
+	private String plateColour;
+	@Column(nullable=false)
+	private String colour;
+	@Column(nullable=false)
+	private String vehicleType;
+	public Vehicle() {
+		
+	}
+	public long getId() {
+		return id;
+	}
+	public DriverProfile getDriverProfile() {
+		return driverProfile;
+	}
+	public void setDriverProfile(DriverProfile driverProfile) {
+		this.driverProfile = driverProfile;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getPlateColour() {
+		return plateColour;
+	}
+	public void setPlateColour(String plateColour) {
+		this.plateColour = plateColour;
+	}
+	public String getColour() {
+		return colour;
+	}
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+	public String getVehicleType() {
+		return vehicleType;
+	}
+	public void setVehicleType(String vehicleType) {
+		this.vehicleType = vehicleType;
+	}
+	
+	
+}

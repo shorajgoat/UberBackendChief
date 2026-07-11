6185ca7979f14bd46c5302e4ccbb75a87063afbd
Shoraj Ghimire <shorajghm10@gmail.com>
2026-07-11 16:43:30 +0545

    Update DriverProfileRequestDto.java

--- src/main/java/com/UberBackend/dto/DriverProfileRequestDto.java
+++ src/main/java/com/UberBackend/dto/DriverProfileRequestDto.java
@@ -11,54 +11,25 @@ public class DriverProfileRequestDto {
 		
 	}
 
-	public DriverProfileRequestDto(String licenseNumber, String model, String plateNumber, String color,
-			String vehicleType) {
-		super();
-		this.licenseNumber = licenseNumber;
-		this.model = model;
-		this.plateNumber = plateNumber;
-		this.color = color;
-		this.vehicleType = vehicleType;
-	}
-
 	public String getLicenseNumber() {
 		return licenseNumber;
-	}
-
-	public void setLicenseNumber(String licenseNumber) {
-		this.licenseNumber = licenseNumber;
 	}
 
 	public String getModel() {
 		return model;
 	}
 
-	public void setModel(String model) {
-		this.model = model;
-	}
-
 	public String getPlateNumber() {
 		return plateNumber;
-	}
-
-	public void setPlateNumber(String plateNumber) {
-		this.plateNumber = plateNumber;
 	}
 
 	public String getColor() {
 		return color;
 	}
 
-	public void setColor(String color) {
-		this.color = color;
-	}
-
 	public String getVehicleType() {
 		return vehicleType;
 	}
 
-	public void setVehicleType(String vehicleType) {
-		this.vehicleType = vehicleType;
-	}
 	
 }

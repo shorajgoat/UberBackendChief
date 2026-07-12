00246ae20443c2735e6bc9117f9887a9ce3fa72b
Shoraj Ghimire <shorajghm10@gmail.com>
2026-07-12 21:05:48 +0545

    Update DriverProfileResponseDto.java

--- src/main/java/com/UberBackend/dto/DriverProfileResponseDto.java
+++ src/main/java/com/UberBackend/dto/DriverProfileResponseDto.java
@@ -13,6 +13,50 @@ public class DriverProfileResponseDto {
 	private String vehicleType;
 	private DriverStatus status;
 	private Integer totalRides;
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setLicenseNumber(String licenseNumber) {
+		this.licenseNumber = licenseNumber;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public void setPlateNumber(String plateNumber) {
+		this.plateNumber = plateNumber;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public void setVehicleType(String vehicleType) {
+		this.vehicleType = vehicleType;
+	}
+
+	public void setStatus(DriverStatus status) {
+		this.status = status;
+	}
+
+	public void setTotalRides(Integer totalRides) {
+		this.totalRides = totalRides;
+	}
+
+	public void setRating(Double rating) {
+		this.rating = rating;
+	}
+
 	private Double rating;
 	
 	public DriverProfileResponseDto() {

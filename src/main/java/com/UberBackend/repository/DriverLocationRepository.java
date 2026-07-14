79b34153f3c8520da739a8303e3c77a29504620c
Shoraj Ghimire <shorajghm10@gmail.com>
2026-07-14 18:34:11 +0545

    Create DriverLocationRepository.java

---
+++ src/main/java/com/UberBackend/repository/DriverLocationRepository.java
@@ -0,0 +1,14 @@
+package com.UberBackend.repository;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.UberBackend.Entity.DriverLocation;
+public interface DriverLocationRepository extends JpaRepository<DriverLocation,Long>{
+	Optional<DriverLocation>findByUserId(Long id);
+	@Query("SELECT dl FROM DriverLocation dl WHERE dl.isAvailable=true")
+	List<DriverLocation>findAllAvailableDrivers();
+	
+}

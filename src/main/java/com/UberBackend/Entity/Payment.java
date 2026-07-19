55ff19a2810c30463dd29bcb74a4e3ebde7c8ea3
Shoraj Ghimire <shorajghm10@gmail.com>
2026-07-19 16:06:07 +0545

    Create Payment.java

---
+++ src/main/java/com/UberBackend/Entity/Payment.java
@@ -0,0 +1,70 @@
+package com.UberBackend.Entity;
+
+import java.time.LocalDateTime;
+
+import com.UberBackend.enums.PaymentStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="payments")
+public class Payment {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "ride_id", nullable = false)
+    private Ride ride;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private Double amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus status;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.status = PaymentStatus.PENDING;
+    }
+
+    // No-Args Constructor
+    public Payment() {}
+
+    // Getters and Setters
+    public Long getId() { return id; }
+
+    public Ride getRide() { return ride; }
+    public void setRide(Ride ride) { this.ride = ride; }
+
+    public User getRider() { return user; }
+    public void setRider(User rider) { this.user = rider; }
+
+    public Double getAmount() { return amount; }
+    public void setAmount(Double amount) { this.amount = amount; }
+
+    public PaymentStatus getStatus() { return status; }
+    public void setStatus(PaymentStatus status) { this.status = status; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+}

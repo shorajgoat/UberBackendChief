1b23d1d85ed369889342067c988895c4f997a350
Shoraj Ghimire <shorajghm10@gmail.com>
2026-07-19 16:10:41 +0545

    Create PaymentRepository.java

---
+++ src/main/java/com/UberBackend/repository/PaymentRepository.java
@@ -0,0 +1,17 @@
+package com.UberBackend.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.UberBackend.Entity.Payment;
+
+
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByRideId(Long rideId);
+    List<Payment> findAllByRiderIdOrderByCreatedAtDesc(Long riderId);
+}

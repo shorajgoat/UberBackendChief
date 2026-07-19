8af399e1ac4f5f4d78d443b094fe2ce07391d0e6
Shoraj Ghimire <shorajghm10@gmail.com>
2026-07-19 17:23:35 +0545

    Update PaymentRepository.java

--- src/main/java/com/UberBackend/repository/PaymentRepository.java
+++ src/main/java/com/UberBackend/repository/PaymentRepository.java
@@ -13,5 +13,5 @@ import com.UberBackend.Entity.Payment;
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Optional<Payment> findByRideId(Long rideId);
-    List<Payment> findAllByRiderIdOrderByCreatedAtDesc(Long riderId);
+    List<Payment> findAllByUserIdOrderByCreatedAtDesc(Long userId);
 }
